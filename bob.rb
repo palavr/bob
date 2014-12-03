ce8062d35bbd65f896aeae615feacda36584d9bc
joshua <jos@sophisticates.de>
2014-12-03 16:05:14 +0100

    odularer

--- bob.rb
+++ bob.rb
@@ -3,23 +3,40 @@ class Bob
 
 def hey(text)
 	
-	if text.strip.empty?
+	if textIsEmpty?(text)
 		return "Fine. Be that way!"
 	end
 	
-	if text == text.upcase
-		# check if text contains letters 
-		if text=~ /[a-zA-Z]/
-			return "Whoa, chill out!"
-		end
+
+	if textYellsAtMe?(text)
+		return "Whoa, chill out!"
 	end
 
-	if text.end_with?("?")
+	if textIsQuestion?(text)
 		return "Sure."
 	end
 	
 	return "Whatever."
 
 end
+
+def textIsEmpty?(text)
+	return text.strip.empty?
+end
+
+def textYellsAtMe?(text)
+	# check if text contains letters 
+	if text=~ /[a-zA-Z]/
+		if text == text.upcase
+			return true
+		end
+	end
+	return false
+end
+
+def textIsQuestion?(text)
+	return text.end_with?("?");
+end
+
 
 end
